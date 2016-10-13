971499c6e15f07c38008a2decb6fdd38ec7b9bb6
Jackson <ace666@mail.ru>
2016-10-13 17:09:04 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,88 @@
+/*var a = 5;
+var b = 2;
+
+
+var str = `the result of division of ${a} by ${b} is ${a/b}`;
+
+console.log(str);*/
+/*
+var a = 5;
+var b = 2;
+function div(a, b) {
+    return a/b;
+}
+
+
+
+var str = `the result of division of ${a} by ${b} is ${div(a, b)}`;
+
+console.log(str);*/
+
+/*var str = `i wanna rock`;
+
+console.log(str.charCodeAt(6));*/
+
+
+/*var arr1 = [1,2,3];
+var arr2 = [4,5,6];
+
+
+var res = arr1.concat(arr2,);
+console.log(res);*/
+
+/*var str = `i wanna rockn`;
+var res = str.indexOf('n', 6);
+
+console.log(res);*/
+
+
+
+/*var str = `i wanna rock`;
+var res = str.slice(2, 9);
+
+console.log(res);*/
+
+
+
+/*var str = `i wanna rock`;
+var res = str.split('a');
+
+console.log(res);*/
+
+/*var st = prompt('input phrase');
+
+function str(string){
+   return string.split(' ').length;
+    
+}
+
+
+alert(str(st));*/
+
+/*var m = [1, 2, 3, 4, 10, 6, 7, 8, 9, 5];
+
+ var count = m.length-1;
+
+for (var i = 0; i < count; i++)
+
+     for (var j = 0; j < count-i; j++)
+
+        if (m[j]> m[j+1]) {
+
+           var max = m[j];
+
+           m[j] = m[j+1];
+
+           m[j+1] = max;
+
+        }
+
+document.write(m);*/
+var st = 'th2is i3s th1e st4ring';
+function str(string) {
+     return string.split(' ');
+}
+
+console.log(str(st));
+
+
